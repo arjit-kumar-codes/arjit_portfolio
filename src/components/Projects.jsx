12fd7e99baa0a8550017f5f33d7aa9251457057a
arjit-kumar-codes <arjit6249@gmail.com>
2025-07-15 09:46:26 +0530

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,7 +38,7 @@ const Projects = () => {
           transition={{ duration: 0.6 }}
           className="text-4xl font-extrabold text-center text-white mb-14"
         >
-          Projects
+          Professional Projects
         </motion.h2>
 
         <div className="grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
